fd845dec540a6583674ac7a4fd2dbcd6e2fbaa02
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2024-01-13 00:13:06 +0700

    Install Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        2: "2px",
+        4: "4px",
+        6: "6px",
+        8: "8px",
+        10: "10px",
+      },
+      borderRadius: {
+        6: "6px",
+        10: "10px",
+        16: "16px",
+      },
+    },
+  },
+  plugins: [],
+};
