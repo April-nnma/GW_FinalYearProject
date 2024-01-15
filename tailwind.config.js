5dd22961466f2d04764ba8151ccbaebcceb71912
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2024-01-15 13:47:31 +0700

    update page login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      spacing: {
-        2: "2px",
-        4: "4px",
-        6: "6px",
-        8: "8px",
-        10: "10px",
-      },
-      borderRadius: {
-        6: "6px",
-        10: "10px",
-        16: "16px",
-      },
+      // spacing: {
+      //   2: "2px",
+      //   4: "4px",
+      //   6: "6px",
+      //   8: "8px",
+      //   10: "10px",
+      // },
+      // borderRadius: {
+      //   6: "6px",
+      //   10: "10px",
+      //   16: "16px",
+      // },
     },
   },
   plugins: [],
